f9747c06f51ce1d806436a0c743622c5f4de4572
sadohu <sadohu26@yahoo.com>
2023-10-21 19:41:46 -0500

    Update: FireBaseConfig - Add: Login, NewUser implement

--- C6_iOS_Project/Views/LoginViewController.swift
+++ C6_iOS_Project/Views/LoginViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtUsuario: UITextField!
+    @IBOutlet weak var txtClave: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -22,7 +26,36 @@ class LoginViewController: UIViewController {
         super.viewWillDisappear(animated)
         CustomConfig.restoreViewController(self)
     }
+    //Actions
     @IBAction func btnNewUser(_ sender: UIButton) {
         performSegue(withIdentifier: "newUserView", sender: self)
-    }
-}
+    }//fin de btnNewUser
+    
+    @IBAction func btnIniciar(_ sender: UIButton) {
+        //leer de las cajas txtUsuario y txtClave
+               let login=txtUsuario.text ?? ""
+               let pass=txtClave.text ?? ""
+               //llamando a loginFirebase y le pasamos las 2 variables que almacenan el valor
+               //de las cajas
+               loginFirebase(usuario: login, clave: pass)
+        
+    }//fin de btnIniciar
+    
+    //creamos func o metodo que tiene de parametro usuario y clave de tipo string
+        func loginFirebase(usuario:String,clave:String){
+            //validar si el usuario(correo electronico) existe
+            Auth.auth().signIn(withEmail: usuario, password: clave) { result, error in
+                //validar result para saber si existe o no existe el usuario en firebase
+                //data almacena lo de result si result tiene datos se ejecuta el if
+                if let data = result{
+                    print("LOGIN CORRECTO")
+                    //imprimimos el valor desde firebase el email y codigo de registro
+                    print(data.user.uid)
+                }//fin de if
+                //si entra al else es que no tiene data
+                else{
+                    print("LOGIN INCORRECTO")
+                }//fin de else
+            } //fin de Auth
+        }//fin de loginFirebase
+}//fin de LogViewController
