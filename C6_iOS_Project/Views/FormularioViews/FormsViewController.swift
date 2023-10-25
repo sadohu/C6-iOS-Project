0e3d642c45dd0aaf6c3e0922c290280475c85757
sadohu <sadohu26@yahoo.com>
2023-10-24 21:27:20 -0500

    [Add] FromView: Capture and Send data from comboBoxes

--- C6_iOS_Project/Views/FormularioViews/FormsViewController.swift
+++ C6_iOS_Project/Views/FormularioViews/FormsViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 //importamos
 import FirebaseFirestore
 
-class FormsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class FormsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, DataLoadedDelegate {
     
     @IBOutlet weak var lblTitulo: UILabel!
     @IBOutlet weak var txtDescripcionTitulo: UITextField!
     @IBOutlet weak var txtUbicacion: UITextField!
-    
-    
-    @IBOutlet weak var txtTipoCateg: UITextField!
-    
     @IBOutlet weak var txtDescrip: UITextField!
     @IBOutlet weak var txtDecripGeneral: UITextField!
     @IBOutlet weak var cboCategoria: UIPickerView!
+    @IBOutlet weak var cboTipo: UIPickerView!
+    
     var titulo: String = ""
     var estado: Int = -1
-    let categorias = ["Categoria 1", "Categoria 2", "Categoria 3"]
+    // Arrays para los cbo
+    var categorias : [Categoria] = [];
+    var listCategoriasFromApi : [Categoria] = [];
+    var listTipos : [Categoria] = [];
+    // Variables de captura del formulario (cbo)
+    var idCategoria : Int = 0;
+    var nombre = "", tipo = "";
+    
     //let categorias = [1, 2, 3]
     //Arreglo de estructura de Publicacion,arreglo de tipo Publicacion en vacio - > []
     var listaPublic:[Publicacion]=[]
@@ -35,10 +40,31 @@ class FormsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         super.viewDidLoad()
         cboCategoria.delegate = self
         cboCategoria.dataSource = self
+        cboTipo.delegate = self;
+        cboTipo.dataSource = self;
         lblTitulo.text = titulo
-    
+        CustomConfig.getCategoriasFromApi(delegate: self);
         //print(estado)
         // Do any additional setup after loading the view.
+    }
+    
+    func dataLoadedSuccessfully(data: [Categoria]) {
+        listCategoriasFromApi = [];
+        listTipos = [];
+        categorias = [];
+        listCategoriasFromApi = data.sorted{ $0.idCategoria < $1.idCategoria };
+        listTipos = listCategoriasFromApi;
+        categorias = CustomConfig.getCategoriasSection(listCategoriasFromApi);
+        cboCategoria.reloadAllComponents();
+    }
+    
+    func updateTipoPicker() {
+        let selectedCategoryRow = cboCategoria.selectedRow(inComponent: 0)
+        if selectedCategoryRow >= 0 && selectedCategoryRow < categorias.count {
+            let selectedCategory = categorias[selectedCategoryRow]
+            listTipos = listCategoriasFromApi.filter{$0.idCategoria == selectedCategory.idCategoria }
+            cboTipo.reloadAllComponents();
+        }
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -46,44 +72,58 @@ class FormsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return categorias.count
+        if pickerView == cboCategoria {
+            return categorias.count;
+        } else if pickerView == cboTipo {
+            return listTipos.count;
+        }
+        return 0;
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return categorias[row]
+        if pickerView == cboCategoria {
+            return categorias[row].nombre;
+        } else if pickerView == cboTipo {
+            return listTipos[row].tipo;
+        }
+        return nil;
     }
-    
-    
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-       // _ = categorias[row]
-        let selectedCategoryRow = row + 1
-        // Puedes guardar la selección en una propiedad de la clase o hacer algo con ella.
+        if pickerView == cboCategoria {
+            let row = (cboCategoria.selectedRow(inComponent: 0));
+            self.idCategoria = categorias[row].idCategoria;
+            self.nombre = categorias[row].nombre;
+            updateTipoPicker();
+        }else if pickerView == cboTipo {
+            let row = (cboTipo.selectedRow(inComponent: 0));
+            self.tipo = listTipos[row].tipo;
+        }
     }
+    
     @IBAction func btnPublicar(_ sender: UIButton) {
+        
         let descripcionTitulo = txtDescripcionTitulo.text
         let nombrepublicaper = txtUbicacion.text
         let descripcion = txtDescrip.text
         let descripPer = txtDecripGeneral.text
-        let tipoCat = txtTipoCateg.text
-        let selectedCategoryRow = cboCategoria.selectedRow(inComponent: 0)
+        //let selectedCategoryRow = cboCategoria.selectedRow(inComponent: 0)
         //let selectedCategory = categorias[selectedCategoryRow]
         //para clave - valor , se muestra categoria1 se guarda 1
-        let selectedCategory = selectedCategoryRow + 1
+        //let selectedCategory = selectedCategoryRow + 1
         // Crea un diccionario con los datos que deseas enviar a Firebase
         let data: [String: Any] = [
             "titulopubli": descripcionTitulo ?? "",
             "nombrepublicaper": nombrepublicaper ?? "",
             "descrippublicaper": descripPer ?? "",
             "descrippubli": descripcion ?? "",
-            "tipo": tipoCat ?? "",
-            "comentinteres" : "",
-            "idCategoria": selectedCategory
+            "tipo": self.tipo,
+            "comentinteres" : self.nombre,
+            "idCategoria": self.idCategoria,
             //"comentinteres": selectedCategory
         ]
-        
         db.collection("publicacion").addDocument(data: data) { error in
-            if let error = error {
+            if error != nil {
                 print("Error al guardar datos")
             } else {
                 print("Datos guardados exitosamente")
